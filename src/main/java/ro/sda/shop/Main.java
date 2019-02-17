143b143d39604c8da0754c672603ef89dd752a45
=Andrei <=balasca.andrei@gmail.com>
2019-02-17 14:33:04 +0200

    new repo

---
+++ src/main/java/ro/sda/shop/Main.java
@@ -0,0 +1,12 @@
+package ro.sda.shop;
+
+import ro.sda.shop.presentation.MainMenu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+        menu.displayMenu();
+    }
+}
+
